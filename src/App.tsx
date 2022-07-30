c7beb727ccf62181f028eb916ca52ce758058a1e
Jeffrey Washburn <jeffrey.s.washburn@outlook.com>
2022-07-30 15:00:00 -0700

    add google signin prompt on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState<any>(null);
+
+  function handleCallbackResponse(
+    response: google.accounts.id.CredentialResponse
+  ) {
+    var userObject = jwt_decode(response.credential);
+    setUser(userObject);
+    console.log(userObject);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id:
+        "306912146319-97vc6au4h8jp5himt0jgtq3c6uss832o.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.prompt();
+  }, []);
+
   return (
     <div className="App">
+      {user && (
+        <div className="Profile">
+          <img src={user.picture} />
+          <h2>Signed in!</h2>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
