dc147713d3c5091e3ac56895b7e1865b9f80bf60
Jeffrey Washburn <jeffrey.s.washburn@outlook.com>
2022-07-31 13:36:49 -0700

    show login button if prompt is cancelled

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -7,20 +7,33 @@ export default function App() {
   function handleCallbackResponse(
     response: google.accounts.id.CredentialResponse
   ) {
+    console.log(response.credential);
     var userObject = jwt_decode(response.credential);
     setUser(userObject);
     console.log(userObject);
   }
 
   useEffect(() => {
-    /* global google */
     google.accounts.id.initialize({
       client_id:
         "306912146319-97vc6au4h8jp5himt0jgtq3c6uss832o.apps.googleusercontent.com",
       callback: handleCallbackResponse,
     });
 
-    google.accounts.id.prompt();
+    if (user) return;
+
+    google.accounts.id.prompt((notification) => {
+      if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+        google.accounts.id.renderButton(document.getElementById("signInDiv")!, {
+          type: "standard",
+          shape: "pill",
+          size: "large",
+          theme: "filled_blue",
+          text: "continue_with",
+          logo_alignment: "center",
+        });
+      }
+    });
   }, []);
 
   return (
@@ -31,6 +44,7 @@ export default function App() {
           <h2>Signed in!</h2>
         </div>
       )}
+      {!user && <div id="signInDiv"></div>}
     </div>
   );
 }
